9459093fda9a5efee4390bab7b33d559684c0d8d
Tom Gilmour <tom.gilmour23@imperial.ac.uk>
2025-06-11 19:34:17 +0100

    hard coded near optimal version

--- HopfieldNetwork/HopfieldNetwork.py
+++ HopfieldNetwork/HopfieldNetwork.py
@@ -31,21 +31,39 @@ class StartNeuron(Neuron):
 
     def ac(self, network, weight=0.1):
         if self.op != 1:
-            mul = network.getChar()
-            ind = (mul[0]*(self.job-1) + mul[2]*(self.machine-1) - 1)
-            self.threshold = network.getStart()[ind].getEnd()
+            self.threshold = network.getStart()[
+                4*(self.job-1)+self.op-2].getEnd()
         sum1 = 0
         for pair in network.getSch():
-            sum1 += pair.getState()*weight
+            c1 = (pair.n1.getLocation() == self.getLocation())
+            c2 = (pair.n2.getLocation() == self.getLocation())
+            if c1:
+                sum1 += pair.getState()[1]*0.05
+            elif c2:
+                sum1 += pair.getState()[0]*0.05
         sum2 = 0
         for pair in network.getRes():
-            sum2 += pair.getState()*weight
+            c1 = (pair.n1.getLocation() == self.getLocation())
+            c2 = (pair.n2.getLocation() == self.getLocation())
+            if c1:
+                if pair.getState()[2]:
+                    sum2 += pair.getState()[1]*0.01
+                else:
+                    sum2 += pair.getState()[0]*0.01
+            elif c2:
+                if pair.getState()[2]:
+                    sum2 += pair.getState()[0]*0.01
+                else:
+                    sum2 += pair.getState()[1]*0.01
         N = sum1 + sum2 + self.state
-        if N <= self.threshold:
+        if N < self.threshold:
             self.hold = self.threshold
             return self.threshold
         self.hold = N
         return N
+
+    def output(self):
+        return (round(self.state), (self.job, self.op, self.machine))
 
     def getEnd(self):
         return self.state + self.time
@@ -62,49 +80,36 @@ class StartNeuron(Neuron):
 
 class ScheduleNeuron(Neuron):
     def __init__(self, n1, n2, state=0):
-        super().__init__(state)
+        self.state = (state, state)
         self.n1 = n1
         self.n2 = n2
         self.location = (n1.getLocation(), n2.getLocation())
 
     def ac(self, network):
         # update incoming neuron states
-        mul = network.getChar()
-        ind1 = 0
-        ind2 = 0
-        for i in range(3):
-            ind1 += mul[i]*(self.location[0][i] - 1)
-            ind2 += mul[i]*(self.location[1][i] - 1)
+        ind1 = 4*(self.location[0][0]-1) + self.location[0][1] - 1
+        ind2 = 4*(self.location[1][0]-1) + self.location[1][1] - 1
         self.n1, self.n2 = network.getStart()[ind1], network.getStart()[ind2]
-        # orders neurons by operation order
-        if self.location[0][1] < self.location[1][1]:
-            first, last = self.n1, self.n2
-        else:
-            first, last = self.n2, self.n1
         # applies the activation formula
-        N = first.getState() + first.getTime() - last.getState()
-        if N < 0:
-            self.hold = 0
-            return 0
-        self.hold = N
-        return N
+        N = self.n1.getState() + self.n1.getTime() - self.n2.getState()
+        if N <= 0:
+            self.hold = (0, 0)
+            return (0, 0)
+        self.hold = (N, -1*N)
+        return (N, -1*N)
 
 
 class ResourceNeuron(Neuron):
     def __init__(self, n1, n2, state=0):
-        super().__init__(state)
+        self.state = (state, state, True)
         self.n1 = n1
         self.n2 = n2
         self.location = (n1.getLocation(), n2.getLocation())
 
     def ac(self, network):
         # update input neurons
-        mul = network.getChar()
-        ind1 = 0
-        ind2 = 0
-        for i in range(3):
-            ind1 += mul[i]*(self.location[0][i] - 1)
-            ind2 += mul[i]*(self.location[1][i] - 1)
+        ind1 = 4*(self.location[0][0]-1) + self.location[0][1] - 1
+        ind2 = 4*(self.location[1][0]-1) + self.location[1][1] - 1
         self.n1, self.n2 = network.getStart()[ind1], network.getStart()[ind2]
         # order neurons by start time
         if self.n1.getState() < self.n2.getState():
@@ -113,39 +118,19 @@ class ResourceNeuron(Neuron):
             first, last = self.n2, self.n1
         # applies the activation formula
         N = last.getEnd() - first.getState()
-        if N < first.getTime() + last.getTime():
-            self.hold = N
-            return N
-        self.hold = 0
-        return 0
+        if N <= first.getTime() + last.getTime():
+            self.hold = (N, -1*N, first == self.n1)
+            return self.hold
+        self.hold = (0, 0, first == self.n1)
+        return self.hold
 
 
 class Network:
-    def __init__(self, timeList, job, op, machine):
-        self.character = (machine*op, machine * (op - 1), machine - 1)
-        self.startNeurons = [StartNeuron(mt.floor(i/(op*machine)) + 1,
-                                         (mt.floor(i/machine) % op) + 1,
-                                         (i % machine) + 1,
-                                         timeList[i])
-                             for i in range(len(timeList))]
-        for i in range(len(timeList)):
-            if (mt.floor(i/machine) % op) != 0:
-                self.startNeurons[i].setThresh(self.startNeurons[
-                    i-machine].getEnd())
-        self.resourceNeurons = []
-        for k in range(machine):
-            for i in range(job * op):
-                for p in range(i + 1, job * op):
-                    self.resourceNeurons.append(ResourceNeuron(
-                        self.startNeurons[(i*machine+k)],
-                        self.startNeurons[(p*machine+k)]))
-        self.scheduleNeurons = []
-        for i in range(job):
-            for j in range(op * machine):
-                for a in range(j + 1, op * machine):
-                    self.scheduleNeurons.append(ScheduleNeuron(
-                        self.startNeurons[(i*(op * machine)+j)],
-                        self.startNeurons[(i*(op * machine)+a)]))
+    def __init__(self, Starts, Schedules, Resource):
+        self.character = (3, 1, 0)
+        self.startNeurons = Starts
+        self.scheduleNeurons = Schedules
+        self.resourceNeurons = Resource
 
     def round(self):
         for n in self.startNeurons:
--- test.py
+++ test.py
@@ -6,27 +6,64 @@ sn2 = HN.StartNeuron(1, 2, 1, 3, state=0)
 sc = HN.ScheduleNeuron(sn_test, sn2)
 rn = HN.ResourceNeuron(sn_test, sn2)
 
-timelist = [1, 2,
-            2, 3,
-            5, 2,
-            4, 1,
-            4, 2,
-            3, 1]
-myNet = HN.Network(timelist, 3, 2, 2)
+timelist = [2, 0,
+            0, 2,
+            0, 1,  # job 1
+            0, 2,
+            0, 1,
+            2, 0,  # job 2
+            1, 0,
+            4, 0,
+            1, 0]  # job 3
+
+
+starts = [HN.StartNeuron(1, 1, 1, 4),
+          HN.StartNeuron(1, 2, 2, 1),
+          HN.StartNeuron(1, 3, 3, 7),
+          HN.StartNeuron(1, 4, 4, 7),
+          HN.StartNeuron(2, 1, 2, 3),
+          HN.StartNeuron(2, 2, 4, 7),
+          HN.StartNeuron(2, 3, 1, 2),
+          HN.StartNeuron(2, 4, 3, 8),
+          HN.StartNeuron(3, 1, 4, 2),
+          HN.StartNeuron(3, 2, 2, 2),
+          HN.StartNeuron(3, 3, 1, 7),
+          HN.StartNeuron(3, 4, 3, 2)]
+
+schedules = [HN.ScheduleNeuron(starts[0], starts[1]),
+             HN.ScheduleNeuron(starts[1], starts[2]),
+             HN.ScheduleNeuron(starts[2], starts[3]),
+             HN.ScheduleNeuron(starts[4], starts[5]),
+             HN.ScheduleNeuron(starts[5], starts[6]),
+             HN.ScheduleNeuron(starts[6], starts[7]),
+             HN.ScheduleNeuron(starts[8], starts[9]),
+             HN.ScheduleNeuron(starts[9], starts[10]),
+             HN.ScheduleNeuron(starts[10], starts[11])]
+
+rescourse = [HN.ResourceNeuron(starts[0], starts[6]),
+             HN.ResourceNeuron(starts[0], starts[10]),
+             HN.ResourceNeuron(starts[6], starts[10]),
+             HN.ResourceNeuron(starts[1], starts[4]),
+             HN.ResourceNeuron(starts[1], starts[9]),
+             HN.ResourceNeuron(starts[4], starts[9]),
+             HN.ResourceNeuron(starts[2], starts[7]),
+             HN.ResourceNeuron(starts[2], starts[11]),
+             HN.ResourceNeuron(starts[7], starts[11]),
+             HN.ResourceNeuron(starts[3], starts[5]),
+             HN.ResourceNeuron(starts[3], starts[8]),
+             HN.ResourceNeuron(starts[5], starts[8])]
+
+myNet = HN.Network(starts, schedules, rescourse)
 
 rounds = []
-rounds.append([i.getState() for i in myNet.getStart()])
+rounds.append([i.output() for i in myNet.getStart()])
 myNet.round()
-rounds.append([i.getState() for i in myNet.getStart()])
-myNet.round()
-rounds.append([i.getState() for i in myNet.getStart()])
-myNet.round()
-myNet.round()
-myNet.round()
-myNet.round()
-myNet.round()
-rounds.append([i.getState() for i in myNet.getStart()])
+rounds.append([i.output() for i in myNet.getStart()])
+for i in range(5000):
+    myNet.round()
+rounds.append([i.output() for i in myNet.getStart()])
+outputList = [i.output()[0] for i in myNet.getStart()]
 print(rounds[0])
 print(rounds[1])
-print(rounds[2])
-print(rounds[3])
+print(rounds[-1])
+print(outputList)
